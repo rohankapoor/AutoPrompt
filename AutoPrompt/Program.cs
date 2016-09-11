b617ccddf1f919fdc4b3c07c1ec08d0788793e03
Rohan Kapoor <rohankapoor12@gmail.com>
2016-09-12 00:14:55 +0530

    Renaming namespace

--- AutoPrompt/Program.cs
+++ AutoPrompt/Program.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AutoPrompt
+namespace rohankapoor.AutoPrompt.Demo
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
         }
     }
