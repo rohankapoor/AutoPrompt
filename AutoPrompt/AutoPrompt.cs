c96380e679d3c3100ddf8a2b3cfdd28fec018a8f
Rohan Kapoor <rohankapoor12@gmail.com>
2016-09-12 14:02:21 +0530

    Adding APIs: 1. PromptForInput (msg, initial input) 2. PromptForInput (msg, input array) 3. PromptForInput_searchable (msg, input array) 4. GetPassword

--- AutoPrompt/AutoPrompt.cs
+++ AutoPrompt/AutoPrompt.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace rohankapoor.AutoPrompt
 {
     public class AutoPrompt
     {
-        // Used to get path from command prompt
+        #region Public APIs
+
+        /// <summary>
+        /// Used to get path from command prompt. Starts by showing available directories
+        /// Press:
+        ///      Up Arrow to go to next file or directory
+        ///      Down Arrow to go to previous file or directory
+        ///      '\' to navigate into current directory
+        ///      Any key to list matching files and directory. The search criteria is 'begins with'. Eg, When 'W' is pressed in C:\, the prompt will auto fill with 'Windows' as the closest match. The search is case sensitive
+        /// </summary>
+        /// <param name="message">Message that appears on the prompt. For eg, Enter Value:</param>
+        /// <returns>Full path of the file or directory selected</returns>
         public static string GetPath(string message)
         {
             string[] options = Environment.GetLogicalDrives();
@@ -154,6 +162,306 @@ namespace rohankapoor.AutoPrompt
             return userInput;
         }
 
+        /// <summary>
+        /// Asks the user for input, with a default input value already set
+        /// </summary>
+        /// <param name="message">Message that appears on the prompt. For eg, Enter Value:</param>
+        /// <param name="initialInput">The value prefilled. Eg, "22 Jump Street" prefilled if prompt is to accept address</param>
+        /// <returns>User entered string or 'initialInput' if no edits were made</returns>
+        public static string PromptForInput(string message, string initialInput)
+        {
+            string userInput = initialInput;
+
+            ConsoleKeyInfo KeyInfo;
+
+            Console.Write(message);
+            Console.Write(initialInput);
+
+            do
+            {
+                // Getting a Key from console
+                KeyInfo = Console.ReadKey(true);
+
+                if (Char.IsLetterOrDigit(KeyInfo.KeyChar) || Char.IsSymbol(KeyInfo.KeyChar)
+                    || Char.IsPunctuation(KeyInfo.KeyChar) || Char.IsPunctuation(KeyInfo.KeyChar)
+                    || Char.IsWhiteSpace(KeyInfo.KeyChar))
+                {
+                    // If its a Letter or Digit, echo to screen, add to Input String
+                    Console.Write(KeyInfo.KeyChar);
+
+                    userInput += KeyInfo.KeyChar;
+                }
+                else if (!userInput.Equals(String.Empty) && KeyInfo.Key == ConsoleKey.Backspace)
+                {
+                    //User hit back space
+
+                    // Erasing on Console
+                    Console.Write("\b" + " " + "\b");
+
+                    // Erasing in Input String
+                    if (!String.IsNullOrEmpty(userInput))
+                    {
+                        userInput = userInput.Substring(0, userInput.Length - 1);
+                    }
+                }
+            }
+            while (KeyInfo.Key != ConsoleKey.Enter);
+
+            return userInput.Trim();
+        }
+
+        /// <summary>
+        /// Accepts user input by displaying options. The input is editable as well
+        /// Press:
+        ///     Up Arrow to go to next option
+        ///     Down Arrow to go to previous option
+        /// </summary>
+        /// <param name="message">Message that appears on the prompt. For eg, Enter Value:</param>
+        /// <param name="options">List of values. Eg {Mr, Mrs, Ms} can be passed if prompt if for salutation. User can switch between these values by pressing up/down arrow OR enter something like Dr or Er and hit enter</param>
+        /// <returns>User entered string or one of the string in 'options' choosen by the user</returns>
+        public static string PromptForInput(string message, string[] options)
+        {
+            int OptionCount = options.Length;
+            int CurrentOptionDisplayedIndex = 0;
+            string UserInput = options[0];
+
+            ConsoleKeyInfo KeyInfo;
+
+            Console.Write(message + options[0]);
+
+            do
+            {
+                KeyInfo = Console.ReadKey(true);
+
+                if (Char.IsLetterOrDigit(KeyInfo.KeyChar) || Char.IsSymbol(KeyInfo.KeyChar)
+                    || Char.IsPunctuation(KeyInfo.KeyChar) || Char.IsPunctuation(KeyInfo.KeyChar)
+                    || Char.IsWhiteSpace(KeyInfo.KeyChar))
+                {
+                    Console.Write(KeyInfo.KeyChar);
+
+                    UserInput += KeyInfo.KeyChar;
+                }
+                else if (!UserInput.Equals(String.Empty) && KeyInfo.Key == ConsoleKey.Backspace)
+                {
+                    Console.Write("\b" + " " + "\b");
+
+                    if (!String.IsNullOrEmpty(UserInput))
+                    {
+                        UserInput = UserInput.Substring(0, UserInput.Length - 1);
+                    }
+                }
+                else if (KeyInfo.Key == ConsoleKey.UpArrow || KeyInfo.Key == ConsoleKey.DownArrow)
+                {
+                    // We have to display another option
+
+                    // Erase on screen and in UserInput string
+                    for (int UserInputIndex = 0; UserInputIndex < UserInput.Length; UserInputIndex++)
+                    {
+                        Console.Write("\b" + " " + "\b");
+                    }
+
+                    // Display new option
+                    if (KeyInfo.Key == ConsoleKey.UpArrow)
+                    {
+                        if (CurrentOptionDisplayedIndex > 0)
+                        {
+                            CurrentOptionDisplayedIndex--;
+                        }
+                        else
+                        {
+                            CurrentOptionDisplayedIndex = OptionCount - 1;
+                        }
+                    }
+                    else if (KeyInfo.Key == ConsoleKey.DownArrow)
+                    {
+                        if (CurrentOptionDisplayedIndex < OptionCount - 1)
+                        {
+                            CurrentOptionDisplayedIndex++;
+                        }
+                        else
+                        {
+                            CurrentOptionDisplayedIndex = 0;
+                        }
+                    }
+
+                    // Show the new option
+                    Console.Write(options[CurrentOptionDisplayedIndex]);
+
+                    // Set new user input
+                    UserInput = options[CurrentOptionDisplayedIndex];
+                }
+            }
+            while (KeyInfo.Key != ConsoleKey.Enter);
+
+            return UserInput.Substring(0, UserInput.Length - 1);
+        }
+
+        /// <summary>
+        /// Accepts user input by displaying options. The options are autofilled with matches from key press. This is like auto suggest for a text box
+        /// Press:
+        ///     Up Arrow to go to next option
+        ///     Down Arrow to go to previous option
+        ///     Any key to list closest matching option. The search criteria is 'begins with'. Eg, When 'C' is pressed and options is a list of US states, then 'California' is set to as choosen option. The search is case sensitive
+        /// </summary>
+        /// <param name="message">Message that appears on the prompt. For eg, Enter Value:</param>
+        /// <param name="options">Input options that the user can choose from using up-down arrow or type to get to</param>
+        /// <returns>User entered string or one of the string in 'options' choosen by the user</returns>
+        public static string PromptForInput_Searchable(string message, string[] options)
+        {
+            int OptionCount = options.Length;
+            int CurrentOptionDisplayedIndex = 0;
+            string UserInput = options[0];
+
+            ConsoleKeyInfo KeyInfo;
+
+            Console.Write(message + options[0]);
+
+            do
+            {
+                KeyInfo = Console.ReadKey(true);
+
+                if (Char.IsLetterOrDigit(KeyInfo.KeyChar) || Char.IsSymbol(KeyInfo.KeyChar)
+                    || Char.IsPunctuation(KeyInfo.KeyChar) || Char.IsPunctuation(KeyInfo.KeyChar)
+                    || Char.IsWhiteSpace(KeyInfo.KeyChar))
+                {
+                    Console.Write(KeyInfo.KeyChar);
+
+                    UserInput += KeyInfo.KeyChar;
+
+                    // Performing Closest Search
+                    int FoundIndex = -1;
+                    int OptionIndex = 0;
+                    foreach (string Option in options)
+                    {
+                        if (Option.ToLower().StartsWith(UserInput.ToLower()))
+                        {
+                            FoundIndex = OptionIndex;
+
+                            /* Display that Option */
+                            /* Erasing Old */
+                            for (int UserInputIndex = 0; UserInputIndex < UserInput.Length; UserInputIndex++)
+                            {
+                                Console.Write("\b" + " " + "\b");
+                            }
+
+                            /* display new */
+                            UserInput = options[FoundIndex];
+                            CurrentOptionDisplayedIndex = FoundIndex;
+
+                            Console.Write(options[CurrentOptionDisplayedIndex]);
+
+                            break;
+                        }
+                        OptionIndex++;
+                    }
+                    // Closest search ends
+                }
+                else if (!UserInput.Equals(String.Empty) && KeyInfo.Key == ConsoleKey.Backspace)
+                {
+                    Console.Write("\b" + " " + "\b");
+
+                    if (!string.IsNullOrEmpty(UserInput))
+                    {
+                        UserInput = UserInput.Substring(0, UserInput.Length - 1);
+                    }
+                }
+                else if (!UserInput.Equals(String.Empty) && KeyInfo.Key == ConsoleKey.Delete)
+                {
+                    // Erase it all
+                    for (int UserInputIndex = 0; UserInputIndex < UserInput.Length; UserInputIndex++)
+                    {
+                        Console.Write("\b" + " " + "\b");
+                    }
+
+                    // Set UserInput to Empty
+                    UserInput = "";
+
+                }
+                else if (KeyInfo.Key == ConsoleKey.UpArrow || KeyInfo.Key == ConsoleKey.DownArrow)
+                {
+                    // Display another option
+
+                    // Erase on screen and in UserInput string
+                    for (int UserInputIndex = 0; UserInputIndex < UserInput.Length; UserInputIndex++)
+                    {
+                        Console.Write("\b" + " " + "\b");
+                    }
+
+                    // Display new option
+                    if (KeyInfo.Key == ConsoleKey.UpArrow)
+                    {
+                        if (CurrentOptionDisplayedIndex > 0)
+                            CurrentOptionDisplayedIndex--;
+                        else
+                            CurrentOptionDisplayedIndex = OptionCount - 1;
+
+                    }
+                    else if (KeyInfo.Key == ConsoleKey.DownArrow)
+                    {
+                        if (CurrentOptionDisplayedIndex < OptionCount - 1)
+                            CurrentOptionDisplayedIndex++;
+                        else
+                            CurrentOptionDisplayedIndex = 0;
+                    }
+
+                    // Show new option
+                    Console.Write(options[CurrentOptionDisplayedIndex]);
+
+                    // Set new user input
+                    UserInput = options[CurrentOptionDisplayedIndex];
+                }
+            }
+            while (KeyInfo.Key != ConsoleKey.Enter);
+
+            return UserInput;
+        }
+
+        /// <summary>
+        /// Gets a password without displaying it on screen. Input is masked by *s
+        /// </summary>
+        /// <param name="message">Message that appears on the prompt. For eg, Enter Value:</param>
+        /// <returns>The password entered by the user</returns>
+        public static string GetPassword(string message)
+        {
+            Console.Write(message);
+
+            string PasswordString = "";
+            ConsoleKeyInfo KeyInfo;
+
+            do
+            {
+                // Getting a Key from console
+                KeyInfo = Console.ReadKey(true);
+
+                if (Char.IsLetterOrDigit(KeyInfo.KeyChar))
+                {
+                    // If its a Letter or Digit, echo * to screen, add to Input String
+                    Console.Write("*");
+                    PasswordString += KeyInfo.KeyChar;
+                }
+                else if (PasswordString != String.Empty && KeyInfo.Key == ConsoleKey.Backspace)
+                {
+                    // User hit back space
+
+                    // Erasing on Console
+                    Console.Write("\b" + " " + "\b");
+
+                    // Erasing in Input String
+                    if (!string.IsNullOrEmpty(PasswordString))
+                    {
+                        PasswordString = PasswordString.Substring(0, PasswordString.Length - 1);
+                    }
+                }
+            }
+            while (KeyInfo.Key != ConsoleKey.Enter);
+
+            return PasswordString;
+        }
+
+        #endregion
+
+        #region Utils
+
         private static void Erase(string userInput)
         {
             for (int i = 0; i < userInput.Length; i++)
@@ -182,5 +490,7 @@ namespace rohankapoor.AutoPrompt
 
             return result;
         }
+
+        #endregion
     }
 }
