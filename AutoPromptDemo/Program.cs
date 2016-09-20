ac07d813f8c9ed8c7b8b16645eb4e689bd42e0db
Rohan Kapoor <rohankapoor12@gmail.com>
2016-09-21 00:18:37 +0530

    Adding demo of APIs

--- AutoPromptDemo/Program.cs
+++ AutoPromptDemo/Program.cs
@@ -15,9 +15,26 @@ namespace rohankapoor.AutoPrompt.Demo
 
         private static void PromptForPath()
         {
-            var userInput = AutoPrompt.GetPath("Enter path: ");
+            Console.WriteLine("                         Welcome to demo of AutoPrompt APIs");
+            Console.WriteLine("\n\n1. Demo of prompt with initial input:\n");
+            var userInput1 = AutoPrompt.PromptForInput("Q: How do you feel? (Press backspce to edit)\nA: ", "Awesome");
+            Console.WriteLine("User feels '" + userInput1+ "'");
 
-            Console.WriteLine("Path entered by user : " + userInput);
+            Console.WriteLine("\n\n2. Demo of prompt with options:\n");
+            var userInput2 = AutoPrompt.PromptForInput("Q: Choose occupation (Press up/down arrow to choose)\nA: ", new string[] { "Engineer", "Scientist", "Salesman", "Manager", "TopManagement" });
+            Console.WriteLine("Occupation selected: '" + userInput2 + "'");
+
+            Console.WriteLine("\n\n3. Demo of prompt with searchable options:\n");
+            var userInput3 = AutoPrompt.PromptForInput_Searchable("Q: Choose city (Press up/down arrow or type name)\nA: ", new string[] { "New York City", "San Fransisco", "New Delhi", "Bangalore", "Tokyo" });
+            Console.WriteLine("City selected: '" + userInput3 + "'");
+
+            Console.WriteLine("\n\n4. Demo of prompt for password:\n");
+            var userInput4 = AutoPrompt.GetPassword("Q: Enter password: ");
+            Console.WriteLine("Password entered: '" + userInput4 + "'");
+
+            Console.WriteLine("\n\n5. Demo of prompt for file/folder:\n");
+            var userInput5 = AutoPrompt.GetPath("Q: Enter path of file:\n\tUp/down arrow to go through files in current directory.\n\t'\\' to step into directory\n\tBackspace to edit\n\tOr just type to go to nearest search match\n\nA: ");
+            Console.WriteLine("Path entered: '" + userInput5 + "'");
 
             Console.ReadLine();
         }
